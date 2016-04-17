24b843b9d7b2a8cde773001b59dc8ff27812254b
pdrmdrs <pdr_mdrs@hotmail.com>
2016-04-17 03:40:10 -0300

    added 'argc' and 'argv' for selection purposes

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string.h>
 
 #include "Questoes/Questao01/Questao01.h"
 
@@ -8,22 +9,59 @@ using namespace std;
 
 int main(int argc, const char * argv[]){
 
-	cout << "Initializing tests..." << endl;
+	if(argc > 1){
+		if(strcmp(argv[1], "1") == 0){//question 1
+			cout << "Question 01:" << endl;
+			cout << "Initializing tests..." << endl;
 
-	bool passed = true;
+			bool passed = true;
 
-	passed = testStackCreate();
+			passed = testStackCreate();
 
-	if(passed)
-		passed = testPush();
+			if(passed)
+				passed = testPush();
 
-	if(passed)
-		passed = testPop();
+			if(passed)
+				passed = testPop();
 
-	if(passed)
-		passed = testStackDestroy();
+			if(passed)
+				passed = testStackDestroy();
 
-	cout << "Tests finished without errors." << endl;
+			cout << "Tests finished without errors." << endl;
+		}
+
+		if(strcmp(argv[1], "2") == 0){//question 2
+			cout << "Question 02:" << endl;
+			/*cout << "Initializing tests..." << endl;
+
+			bool passed = true;
+
+			passed = testStackCreate();
+
+			if(passed)
+				passed = testPush();
+
+			if(passed)
+				passed = testPop();
+
+			if(passed)
+				passed = testStackDestroy();
+
+			cout << "Tests finished without errors." << endl;*/
+		}
+
+		if(strcmp(argv[1], "3") == 0){//question 3
+			cout << "Question 03:" << endl;
+		}
+
+		if(strcmp(argv[1], "4") == 0){//question 4
+			cout << "Question 04:" << endl;
+		}
+
+		if(strcmp(argv[1], "5") == 0){//question 5
+			cout << "Question 05:" << endl;
+		}
+	}
 
 	return 0;
 }
